164165c3cff5e471b7557b527756e9641f25778f
merlinz01 <na@notaccessible.xyz>
2024-08-20 20:56:14 -0400

    set lower display contrast

--- display.ino
+++ display.ino
@@ -11,6 +11,8 @@ void initDisplay() {
   }
   display.setRotation(2);
   display.setTextColor(WHITE);
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(1);
 }
 
 void drawSelection(int16_t x, int16_t y, int16_t w, int16_t h, bool fill) {
